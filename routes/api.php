1f0e0d562a0035b7fe818a750a58b312fa82971e
peterosoo <posoo617@gmail.com>
2021-02-24 13:27:57 +0300

    CRUD API Using Resources

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,6 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::post('/post', [PostController::class, 'store']);
 
-Route::get('/posts{id}', [PostController::class, 'show']);
-Route::put('/posts{id}', [PostController::class, 'update']);
-Route::delete('/posts{id}', [PostController::class, 'destroy']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
